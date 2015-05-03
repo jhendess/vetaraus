5363c891e37aa0dee6b82cb867252d8391b35c04
Niklas Wolber <niklas.wolber@octopost.eu>
2015-05-03 21:55:55 +0200

    Make Netica.dll available to Windows

--- app.bat
+++ app.bat
@@ -1,1 +1,2 @@
+set PATH=%PATH%;lib
 java -Djava.library.path=lib -cp lib/NeticaJ.jar;target/vetaraus-1.0-SNAPSHOT-jar-with-dependencies.jar de.dhbw.vetaraus.Application --learn P003_learn.csv P003_classify.csv
