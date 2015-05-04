ca510869cfa8f9e7e5f53815b6a048adfa724ca3
Niklas Wolber <niklas.wolber@octopost.eu>
2015-05-04 20:28:54 +0200

    Don't close command prompt on exit

--- app.bat
+++ app.bat
@@ -1,2 +1,3 @@
-set PATH=%PATH%;lib
+SET PATH=%PATH%;lib
 java -Djava.library.path=lib -cp lib/NeticaJ.jar;target/vetaraus-1.0-SNAPSHOT-jar-with-dependencies.jar de.dhbw.vetaraus.Application --learn P003_learn.csv P003_classify.csv
+PAUSE
